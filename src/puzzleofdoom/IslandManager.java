17e350e3393a047fa8843d661be7417266ab17a4
Crunch-Hudson <benjamin.virgo@epitech.eu>
2016-02-11 01:01:55 +0100

    migration

--- src/puzzleofdoom/IslandManager.java
+++ src/puzzleofdoom/IslandManager.java
@@ -10,6 +10,7 @@ public class IslandManager {
 	public Long islandsCount;
 	public Long boardsCount;
 	public Long piecesCount;
+	public int coeffRepartition;
 	static public boolean hasSolution = false;
 
 	/**
@@ -26,6 +27,7 @@ public class IslandManager {
 		piecesCount = pieces;
 		boardsCount = boards;
 		islands = new ArrayList<Island>();
+		coeffRepartition = 2;
 	}
 
 	public void run() {
@@ -38,7 +40,7 @@ public class IslandManager {
 			selection();
 			evaluate(1);
 			merge();
-			//migrate();
+			migrate();
 		}
 		
 		DebugHelper.Log("\nRecapitulatif\n");
@@ -93,4 +95,58 @@ public class IslandManager {
 		DebugHelper.LogWithEnd("MERGE");
 	}
 
+	private void migrate(){
+		boolean needMigration = false;
+		int i = 0;
+		int j = 0;
+		int k = 0;
+		ArrayList<ArrayList<Board>> boardToMigrate = new ArrayList<ArrayList<Board>>();
+		
+		DebugHelper.LogWithStart("MIGRATE");
+		for (Island island : islands){
+			if (island.needToMigrate()==true){
+				needMigration = true;
+				break;
+			}
+		}
+		if (needMigration == true){
+			for (Island island : islands){
+				boardToMigrate.add(island.migrate(islandsCount, coeffRepartition));
+			}
+		}
+		
+		// Arriver ici la liste de liste de board a migrer est remplie
+		for (ArrayList<Board> boardList : boardToMigrate)
+		{
+			for (Island island : islands)
+			{
+				if (i != j)
+				{
+					while (k < coeffRepartition && boardList.size() > 0)
+					{
+						// On ajoute
+						if (j == 9)
+						{
+							DebugHelper.Log("debug");
+						}
+						DebugHelper.Log("Move : ");
+						DebugHelper.Log(boardList.get(k).number.toString());
+						DebugHelper.Log("From isle : "+i);
+						DebugHelper.Log("To isle :"+j);
+						island.boards.add(boardList.get(k));
+						boardList.remove(k);
+						k++;
+					}
+				}
+				k = 0;
+				j++;
+				/*for (int x = 0; x < island.boards.size(); x++)
+					island.boards.get(x).number = (long)i;
+				*/
+			}
+			j = 0;
+			i++;
+		}
+		DebugHelper.LogWithEnd("MIGRATE");
+	}
 }
